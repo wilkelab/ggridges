cd4b41d0e29a70e83c824479ac576aa7d5f8ab75
Claus Wilke <wilke@austin.utexas.edu>
2017-07-10 15:47:21 -0500

    first working version

---
+++ R/ggjoy.R
@@ -0,0 +1,6 @@
+#' ggjoy.
+#'
+#' @name ggjoy
+#' @docType package
+#' @import ggplot2
+NULL
---
+++ R/utils_ggplot2.R
@@ -0,0 +1,24 @@
+# code that needed to be copied from ggplot2
+
+# Name ggplot grid object
+# Convenience function to name grid objects
+#
+# @keyword internal
+ggname <- function(prefix, grob) {
+  grob$name <- grid::grobName(grob, prefix)
+  grob
+}
+
+snakeize <- function(x) {
+  x <- gsub("([A-Za-z])([A-Z])([a-z])", "\\1_\\2\\3", x)
+  x <- gsub(".", "_", x, fixed = TRUE)
+  x <- gsub("([a-z])([A-Z])", "\\1_\\2", x)
+  tolower(x)
+}
+
+snake_class <- function(x) {
+  snakeize(class(x)[1])
+}
+
+
+
