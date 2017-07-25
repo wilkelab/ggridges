d1e0b3a9aa6d02806b7aa0c4b58efe39d3614909
Claus Wilke <wilke@austin.utexas.edu>
2017-07-25 08:54:18 +0200

    new example for stat_binline

--- R/stats.R
+++ R/stats.R
@@ -133,17 +133,23 @@ StatJoy <- ggproto("StatJoy", Stat,
 #'    scale_fill_viridis(begin = 0.3, discrete = TRUE, option = "B") +
 #'    labs(title = "Movie lengths 1990 - 2005")
 #'
-#' # modified after an example provided by Noam Ross
-#' count_data <- data.frame(group = rep(letters[1:5], each = 20),
-#'                          mean = rep(1:5, each = 20))
+#' count_data <- data.frame(group = rep(letters[1:5], each = 10),
+#'                          mean = rep(1:5, each = 10))
 #' count_data$group <- factor(count_data$group, levels = letters[5:1])
 #' count_data$count <- rpois(nrow(count_data), count_data$mean)
-#' ggplot(count_data, aes(x = count, y = group, fill = group)) +
-#'   geom_joy(stat = "binline", binwidth = 1, scale = 0.9) +
-#'   theme_joy() +
-#'   scale_x_continuous(breaks = c(0:12), limits = c(-1, 13), expand = c(0, 0)) +
+#' ggplot(count_data, aes(x = count, y = group, group = group)) +
+#'   geom_joy2(stat = "binline", binwidth = 1, scale = 0.95,
+#'           aes(fill = ifelse(as.numeric(group) %% 2 == 0, "light", "dark"))) +
+#'   geom_text(stat = "bin",
+#'           aes(y = group+0.9*..count../max(..count..),
+#'           label = ifelse(..count..>0, ..count.., "")),
+#'           vjust = 1.2, size = 3, color = "white", binwidth = 1) +
+#'   theme_joy(grid = FALSE) +
+#'   scale_x_continuous(breaks = c(0:12), limits = c(-.5, 13), expand = c(0, 0)) +
 #'   scale_y_discrete(expand = c(0.01, 0)) +
-#'   guides(fill = guide_legend(reverse = TRUE))
+#'   scale_fill_manual(values = c("dark" = "#0000B0", "light" = "#7070D0")) +
+#'   guides(y = "none",
+#'          fill = "none")
 #' @export
 stat_binline <- function(mapping = NULL, data = NULL,
                      geom = "joy", position = "identity",
