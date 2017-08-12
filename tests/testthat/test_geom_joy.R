0126d0692684114ed3f1ee64c397e0b3928b6e28
Claus Wilke <wilke@austin.utexas.edu>
2017-08-11 20:41:50 -0500

    refactoring visual tests

--- tests/testthat/test_geom_joy.R
+++ tests/testthat/test_geom_joy.R
@@ -5,23 +5,17 @@ context("geom_joy")
 # Visual tests ------------------------------------------------------------
 
 test_that("geom_joy draws correctly", {
-  vdiffr::expect_doppelganger("geom_joy basic",
-    ggplot(iris, aes(x = Sepal.Length, y = Species)) +
-      geom_joy()
-  )
+  p <- ggplot(iris, aes(x = Sepal.Length, y = Species)) + geom_joy()
+  vdiffr::expect_doppelganger("geom_joy basic", p)
 
-  vdiffr::expect_doppelganger("geom_joy no trailing lines",
-    ggplot(iris, aes(x = Sepal.Length, y = Species)) +
-      geom_joy(rel_min_height = 0.005)
-  )
+  p <- ggplot(iris, aes(x = Sepal.Length, y = Species)) + geom_joy(rel_min_height = 0.005)
+  vdiffr::expect_doppelganger("geom_joy no trailing lines", p)
 
-  vdiffr::expect_doppelganger("geom_joy scale=3",
-    ggplot(iris, aes(x = Sepal.Length, y = Species)) +
-      geom_joy(scale = 3)
-  )
+  p <- ggplot(iris, aes(x = Sepal.Length, y = Species)) + geom_joy(scale = 3)
 
-  vdiffr::expect_doppelganger("geom_joy2 solid polygons",
-    ggplot(iris, aes(x = Sepal.Length, y = Species)) +
-      geom_joy2()
-  )
+  vdiffr::expect_doppelganger("geom_joy scale=3", p)
+
+  p <- ggplot(iris, aes(x = Sepal.Length, y = Species)) + geom_joy2()
+
+  vdiffr::expect_doppelganger("geom_joy2 solid polygons", p)
 })
