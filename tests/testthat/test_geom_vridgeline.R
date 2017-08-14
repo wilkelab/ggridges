45bc2148db07e3669ee45b47b8fad7a13e4015ff
Claus Wilke <wilke@austin.utexas.edu>
2017-08-14 13:56:59 -0500

    new tests

---
+++ tests/testthat/test_geom_vridgeline.R
@@ -0,0 +1,14 @@
+context("geom_vridgeline")
+
+
+# Visual tests ------------------------------------------------------------
+test_that("visual appearance", {
+  d <- data.frame(y = rep(1:5, 3), x = c(rep(0, 5), rep(1, 5), rep(3, 5)),
+                  width = c(0, 1, 3, 4, 0, 1, 2, 3, 5, 4, 0, 5, 4, 4, 1))
+  p <- ggplot(d, aes(x, y, width = width, group = x)) + geom_vridgeline(fill="lightblue")
+  vdiffr::expect_doppelganger("geom_vridgeline basic use", p)
+
+  p <- ggplot(iris, aes(x=Species, y=Sepal.Width, width = ..density.., fill=Species)) +
+    geom_vridgeline(stat="ydensity", trim=FALSE, alpha = 0.85, scale = 2)
+  vdiffr::expect_doppelganger("geom_vridgeline using stat ydensity", p)
+})
---
+++ tests/testthat/test_scale_cyclical.R
@@ -0,0 +1,36 @@
+context("scale_*_cyclical")
+
+
+test_that("basic tests", {
+  df <- data.frame(x=sample(1:26), y=sample(1:26),
+                   letters)
+  p <- ggplot(df, aes(x, y, label=letters, color=letters)) + geom_text() +
+    scale_color_cyclical(values = c("#F00000", "#0000F0"))
+  d <- layer_data(p)
+
+  # make sure color pattern repeats as expected
+  expect_equal(d$colour, rep(c("#F00000", "#0000F0"), 13))
+
+  # once again, different aesthetic, different cyclical pattern
+  p <- ggplot(df, aes(x, y, label=letters, color=factor(x))) + geom_text() +
+    scale_color_cyclical(values = c("#F00000", "#0000F0", "#F0F000"))
+  d <- layer_data(p)
+
+  # make sure color pattern repeats as expected
+  expect_equal(d$colour[order(d$x)], rep(c("#F00000", "#0000F0", "#F0F000"), 9)[1:26])
+
+})
+
+
+# Visual tests ------------------------------------------------------------
+test_that("visual appearance", {
+  df <- data.frame(x=1:30, y=1:30)
+  p <- ggplot(df, aes(x, y, fill = factor(x))) + geom_point(shape = 21, size = 3) +
+    scale_fill_cyclical(values = c("#F00000", "#00F000", "#0000F0"))
+  vdiffr::expect_doppelganger("scale_fill_cyclical red-green-blue dots, no legend", p)
+
+  p <- ggplot(df, aes(x, y, color = factor(x))) + geom_point(size = 3) +
+    scale_color_cyclical(values = c("#F00000", "#00F000", "#0000F0"), guide = "legend")
+  vdiffr::expect_doppelganger("scale_fill_cyclical red-green-blue dots, with legend", p)
+
+})
